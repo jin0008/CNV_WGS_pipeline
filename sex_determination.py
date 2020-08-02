ebf2d7f8cd855b9a5549953be4634414378c411a
jin0008 <jin0008@hanmail.net>
2020-08-03 08:29:09 +0900

    Add files via upload

---
+++ sex_determination.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+# Author: Julien Buratti
+
+import os
+import glob
+import pysam
+
+sample_l = []
+sex_d = {}
+bams = glob.glob("*.analysisready.bam")
+
+print("\nSex determination script openning.\n")
+
+for i in bams:
+    sample = os.path.basename(i.split(".")[0])
+    sample_l.append(sample)
+    bamfile = pysam.AlignmentFile(i, "rb")
+    sry_count = bamfile.count(contig='chrY', start=2786989, stop=2787603, until_eof=False, read_callback='all')
+    if sry_count >= 50:
+        sex = "M"
+    elif sry_count <= 10:
+        sex = "F"
+    else:
+        sex = "?"
+        sys.exit("/!\\ Ambigous Sex Determination, please check!")    
+    sex_d[sample] = sex
+
+if os.path.isfile("samples.txt"):
+    print("Sex determination was already done. No changes.")
+else: 
+    with open('samples.txt', 'w') as samp:
+        samp.write("sample\tsex\n")
+        
+        for s in sample_l:
+            samp.write(s + "\t" + sex_d[s] + "\n")
+        
+        print("Sex determination done for {} samples.".format(str(len(sample_l))))
+        print("samples.txt generated.")
+
+print("\nSex determination script job done!\n")
