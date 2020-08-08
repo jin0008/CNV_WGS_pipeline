060b462d562ab4395ec0fcd152a0fee2a620d618
jin0008 <jin0008@hanmail.net>
2020-08-08 13:03:19 +0900

    Update cn.mops_cnv_all.r

--- cn.mops_cnv_all.r
+++ cn.mops_cnv_all.r
@@ -12,7 +12,7 @@ print('Working on all...')
 setwd(dir=".")
 
 
-bamDataRanges <- getReadCountsFromBAM(all, WL = 5000, parallel=20)
+bamDataRanges <- getReadCountsFromBAM(all, parallel=20)
 
 resCNMOPS <- cn.mops(bamDataRanges)
 
